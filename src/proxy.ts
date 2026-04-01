e972ea44172bfd2b2821116cc23ac76f03c5db4a
shubham12-prof <shubhsemwal12@gmail.com>
2026-04-01 17:07:44 +0530

    middleware to proxy.ts name change

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,5 +1,16 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth(
+  function proxy(req) {
+    console.log("User is authenticated");
+  },
+  {
+    pages: {
+      signIn: "/",
+    },
+  },
+);
 
 export const config = {
-  matcher: ["/dashboard"],
+  matcher: ["/dashboard/:path*"],
 };
